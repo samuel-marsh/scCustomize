1e2511b9b37b590b9067e1016e857b864255da15
samuel-marsh <samuel.marsh@childrens.harvard.edu>
2021-12-13 09:36:31 -0500

    add vignette compilation script

---
+++ R/Precompile_Vignettes.R
@@ -0,0 +1,5 @@
+# Precompiled vignettes that depend on SeuratData Package or other data
+
+library(knitr)
+
+knitr::knit("vignettes/Read_and_Write_Functions.Rmd.orig", output = "vignettes/Read_and_Write_Functions.Rmd")
