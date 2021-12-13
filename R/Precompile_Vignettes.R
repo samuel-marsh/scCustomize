01ef5806d83b54c17ee59213042c465f77568e78
samuel-marsh <samuel.marsh@childrens.harvard.edu>
2021-12-13 10:13:06 -0500

    fix precompile

--- R/Precompile_Vignettes.R
+++ R/Precompile_Vignettes.R
@@ -3,3 +3,4 @@
 library(knitr)
 
 knitr::knit("vignettes/Read_and_Write_Functions.Rmd.orig", output = "vignettes/Read_and_Write_Functions.Rmd")
+knitr::knit("vignettes/Sequencing_QC_Plots.Rmd.orig", output = "vignettes/Sequencing_QC_Plots.Rmd")
