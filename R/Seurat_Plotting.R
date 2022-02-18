c4721dc7de4f996cd7cb1b16b4ddef6c8a8eb604
samuel-marsh <samuel.marsh@childrens.harvard.edu>
2022-02-18 10:47:14 -0500

    update

--- R/Seurat_Plotting.R
+++ R/Seurat_Plotting.R
@@ -264,8 +264,7 @@ FeaturePlot_DualAssay <- function(
   # Cell Bender normalize check
   if (!paste0("NormalizeData.", assay2) %in% commands) {
     cli_abort(message = c("Assay 2: {assay2} has not been normalized.",
-                      "i" = "Please run `NormalizeData` on this assay before proceeding to visualization."),
-          use_cli_format = TRUE)
+                      "i" = "Please run `NormalizeData` on this assay before proceeding to visualization."))
   }
 
   # Set columns if single feature
