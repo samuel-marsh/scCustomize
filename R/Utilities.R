c3ba9cd85c615c8a8aa863cfec5bc711ed9b6710
samuel-marsh <samuel.marsh@childrens.harvard.edu>
2022-12-16 06:48:31 -0500

    update example

--- R/Utilities.R
+++ R/Utilities.R
@@ -359,9 +359,7 @@ Meta_Numeric <- function(
 #'
 #' @examples
 #' \dontrun{
-#' data_list <- Read10X_GEO(...)
-#' merged <- Merge_Sparse_Data_All(matrix_list = data_list, add_cell_ids = names(data_list),
-#' prefix = TRUE, cell_id_delimiter = "_")
+#' meta_data <- Fetch_Meta(object = pbmc)
 #' }
 #'
 
