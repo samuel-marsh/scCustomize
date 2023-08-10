718c479743ac9735aacb251e10b325635e6596ec
samuel-marsh <samuel.marsh@childrens.harvard.edu>
2023-08-10 08:27:00 -0400

    add barcode rank plot and iterative plot

--- R/QC_Plotting_Seq_10X.R
+++ R/QC_Plotting_Seq_10X.R
@@ -1887,3 +1887,198 @@ Seq_QC_Plot_Alignment_Combined <- function(
   # Print plots
   suppressMessages(print(plot))
 }
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#################### Barcode Rank QC ####################
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+#' Create Barcode Rank Plot
+#'
+#' Plot UMI vs. Barcode Rank with inflection and knee.  Requires input from DropletUtils package.
+#'
+#' @param br_out DFrame output from \code{\link[DropletUtils]{barcodeRanks}}.
+#' @param pt.size point size for plotting, default is 6.
+#' @param plot_title Title for plot, default is "Barcode Ranks".
+#' @param raster_dpi Pixel resolution for rasterized plots, passed to geom_scattermore().
+#' Default is c(1024, 1024).
+#'
+#' @return A ggplot object
+#'
+#' @import cli
+#' @import ggplot2
+#' @importFrom scattermore geom_scattermore
+#' @importFrom cowplot theme_cowplot
+#'
+#' @export
+#'
+#' @concept seq_qc_plotting_basic
+#'
+#' @examples
+#' \dontrun{
+#' mat <- Read10X_h5(filename = "raw_feature_bc_matrix.h5")
+#'
+#' br_results <- DropletUtils::barcodeRanks(mat)
+#'
+#' Barcode_Plot(br_out = br_results)
+#' }
+#'
+
+Barcode_Plot <- function(
+    br_out,
+    pt.size = 6,
+    plot_title = "Barcode Ranks",
+    raster_dpi = c(1024, 1024)
+) {
+  # Check br_out is correct
+  if (!inherits(x = br_out, what = "DFrame")) {
+    cli_abort(message = c("{.code br_out} must be object of class {.field DFrame}.",
+                          "i" = "Ensure {.code br_out} is output of {.code {.field DropletUtils::barcodeRanks}}."))
+  }
+
+  if (!c("knee", "inflection") %in% names(x = br_out@metadata) && !c("rank", "total", "fitted") %in% names(x = br_out@listData)) {
+    cli_abort(message = c("{.code br_out} appears to be missing necessarily information.",
+                          "i" = "Ensure {.code br_out} is output of {.code {.field DropletUtils::barcodeRanks}} and no errors occured when running code."))
+  }
+
+  ggplot(data = data.frame(br_out@listData), aes(x = rank, y = total)) +
+    geom_scattermore(pointsize = pt.size, pixels = raster_dpi) +
+    scale_y_log10() +
+    scale_x_log10() +
+    theme_cowplot() +
+    geom_line(mapping = aes(x = rank, y = fitted, color = "red"), show.legend = FALSE) +
+    geom_hline(yintercept = br_out@metadata$knee, linetype = "dashed", color = "dodgerblue") +
+    geom_hline(yintercept = br_out@metadata$inflection, linetype = "dashed", color = "forestgreen") +
+    annotate("text", x = 1, y = br_out@metadata$knee, label = paste0("Knee (", br_out@metadata$knee, ")"), vjust = -0.5, hjust = 0) +
+    annotate("text", x = 1, y = br_out@metadata$inflection, label = paste0("Inflection (", br_out@metadata$inflection, ")"), vjust = -0.5, hjust = 0) +
+    ylab("UMIs") +
+    xlab("Barcode Rank") +
+    ggtitle(plot_title) +
+    theme(plot.title = element_text(hjust = 0.5))
+}
+
+
+#' Iterative Barcode Rank Plots
+#'
+#' Read data, calculate `DropletUtils::barcodeRanks`, create barcode rank plots, and outout single PDF output.
+#'
+#' @param dir_path_h5 path to parent directory (if `multi_directory = TRUE`) or directory containing
+#' all h5 files (if `multi_directory = FALSE`).
+#' @param multi_directory logical, whether or not all h5 files are in their own subdirectories or in a
+#' single directory (default is TRUE; each in own subdirectory (e.g. output from Cell Ranger)).
+#' @param h5_filename Either the file name of h5 file (if `multi_directory = TRUE`) or the shared
+#' suffix (if `multi_directory = FALSE`)
+#' @param cellranger_multi logical, whether the outputs to be read are from Cell Ranger `multi` as opposed
+#' to Cell Ranger `count` (default is FALSE).  Only valid if `multi_directory = FALSE`.
+#' @param parallel logical, should files be read in parallel (default is FALSE).
+#' @param num_cores Number of cores to use in parallel if `parallel = TRUE`.
+#' @param file_path file path to use for saving PDF output.
+#' @param file_name Name of PDF output file.
+#' @param pt.size point size for plotting, default is 6.
+#' @param raster_dpi Pixel resolution for rasterized plots, passed to geom_scattermore().
+#' Default is c(1024, 1024).
+#' @param ... Additional parameters passed to `Read10X_h5_Multi_Directory` or `Read10X_h5_GEO`.
+#'
+#' @return pdf document
+#'
+#' @import cli
+#' @import ggplot2
+#' @importFrom DropletUtils barcodeRanks
+#' @importFrom grDevices dev.off pdf
+#' @importFrom pbapply pblapply pboptions
+#' @importFrom utils txtProgressBar setTxtProgressBar
+#'
+#' @export
+#'
+#' @concept seq_qc_plotting_basic
+#'
+#' @examples
+#' \dontrun{
+#' Iterate_Barcode_Rank_Plot(dir_path_h5 = "H5_PATH/", multi_directory = TRUE, h5_filename = "raw_feature_bc_matrix",
+#' parallel = TRUE, num_cores = 12, file_path = "OUTPUT_PATH", file_name = "Barcode_Rank_Plots")
+#' }
+#'
+
+Iterate_Barcode_Rank_Plot <- function(
+    dir_path_h5,
+    multi_directory = TRUE,
+    h5_filename = "raw_feature_bc_matrix.h5",
+    cellranger_multi = FALSE,
+    parallel = FALSE,
+    num_cores = NULL,
+    file_path = NULL,
+    file_name = NULL,
+    pt.size = 6,
+    raster_dpi = c(1024, 1024),
+    ...
+) {
+  DropletUtils_check <- PackageCheck("DropletUtils", error = FALSE)
+  if (!DropletUtils_check[1]) {
+    cli_abort(message = c(
+      "Please install the {.val DropletUtils} package to use {.code Create_10X_H5}",
+      "i" = "This can be accomplished with the following commands: ",
+      "----------------------------------------",
+      "{.field `install.packages({symbol$dquote_left}BiocManager{symbol$dquote_right})`}",
+      "{.field `BiocManager::install({symbol$dquote_left}DropletUtils{symbol$dquote_right})`}",
+      "----------------------------------------"
+    ))
+  }
+
+  # Set file_path before path check if current dir specified as opposed to leaving set to NULL
+  if (!is.null(x = file_path) && file_path == "") {
+    file_path <- NULL
+  }
+
+  # Check file path is valid
+  if (!is.null(x = file_path)) {
+    if (!dir.exists(paths = file_path)) {
+      cli_abort(message = "Provided {.code file_path}: {symbol$dquote_left}{.field {file_path}}{symbol$dquote_right} does not exist.")
+    }
+  }
+
+  # Check if file name provided
+  if (is.null(x = file_name)) {
+    cli_abort(message = "No file name provided.  Please provide a file name using {.code file_name}.")
+  }
+
+  # Set file type for single pdf option
+  file_type <- ".pdf"
+
+  # Read in data
+  if (multi_directory) {
+    all_mat <- Read10X_h5_Multi_Directory(base_path = dir_path_h5, h5_filename = h5_filename, parallel = parallel, num_cores = num_cores, ...)
+  } else {
+    all_mat <- Read10X_h5_GEO(data_dir = dir_path_h5, parallel = parallel, num_cores = num_cores, shared_suffix = h5_filename, ...)
+  }
+
+  cli_inform(message = "{.field Calculating Barcode Rank Statistics}")
+  pboptions(char = "=")
+  barcode_ranks_list <- pblapply(1:length(x = all_mat), function(x) {
+    br_file <- barcodeRanks(m = all_mat[[x]])
+  })
+
+  sample_names <- names(x = all_mat)
+
+  rm(all_mat)
+  gc()
+
+  num_samples <- length(x = barcode_ranks_list)
+
+  # Single PDF option
+  cli_inform(message = "{.field Generating plots}")
+  pboptions(char = "=")
+  all_plots <- pblapply(1:num_samples, function(j) {
+    Barcode_Plot(br.out = barcode_ranks_list[[j]], pt.size = pt.size, plot_title = sample_names[j], raster_dpi = raster_dpi)
+  })
+  cli_inform(message = "{.field Saving plots to file}")
+  # Save plots
+  pdf(paste(file_path, file_name, file_type, sep=""))
+  pb <- txtProgressBar(min = 0, max = length(all_plots), style = 3, file = stderr())
+  for (i in 1:length(all_plots)) {
+    print(all_plots[[i]])
+    setTxtProgressBar(pb = pb, value = i)
+  }
+  close(con = pb)
+  dev.off()
+}
