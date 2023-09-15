6113996ee1553f5413bcd6d91255689a32db60bc
samuel-marsh <samuel.marsh@childrens.harvard.edu>
2023-09-15 11:01:48 -0400

    replace superceded .data with all_of

--- R/Statistics_Plotting.R
+++ R/Statistics_Plotting.R
@@ -770,7 +770,7 @@ CellBender_Diff_Plot <- function(
 ) {
   # Remove unshared features
   feature_diff_df_filtered <- feature_diff_df %>%
-    drop_na(.data[["Raw_Counts"]], .data[["CellBender_Counts"]])
+    drop_na(all_of(c("Raw_Counts", "CellBender_Counts")))
 
   diff_features <- symdiff(x = rownames(x = feature_diff_df), y = rownames(x = feature_diff_df_filtered))
 
