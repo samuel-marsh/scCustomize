57bec61171caaa5a1fc1a0b60112d41723cbc5da
samuel-marsh <samuel.marsh@childrens.harvard.edu>
2024-01-12 08:13:19 -0500

    remove as.seurat liger for now

--- R/Generics.R
+++ R/Generics.R
@@ -14,17 +14,3 @@
 as.LIGER <- function(x, ...) {
   UseMethod(generic = 'as.LIGER', object = x)
 }
-
-#' Convert objects to Seurat objects
-#'
-#' @param x An object(s) to convert to class \code{Seurat}
-#' @param ... Arguments passed to other methods
-#'
-#' @rdname as.Seurat
-#' @importMethodsFrom SeuratObject as.Seurat
-#' @export
-#'
-
-as.Seurat <- function(x, ...) {
-  UseMethod(generic = 'as.Seurat', object = x)
-}
