aeb909fac65b39de8ca224c7ec82218f554fb149
samuel-marsh <samuel.marsh@childrens.harvard.edu>
2024-02-15 10:07:22 -0500

    update code link

--- R/Reexports.R
+++ R/Reexports.R
@@ -1,6 +1,6 @@
 #' @importFrom SeuratObject as.Seurat
 #' @export
-#' @note See `as.Seurat.liger()` for scCustomize extension of this generic to converting Liger objects.
+#' @note See \code{\link{as.Seurat.liger}} for scCustomize extension of this generic to converting Liger objects.
 #'
 #'
 SeuratObject::as.Seurat
