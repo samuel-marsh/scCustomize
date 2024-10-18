496ce117b68684aab59ae27fd9e2bfd1c05dc0ad
samuel-marsh <samuel.marsh@childrens.harvard.edu>
2024-10-18 10:41:48 -0400

    message fixes

--- R/Object_Utilities.R
+++ R/Object_Utilities.R
@@ -1472,8 +1472,8 @@ Random_Cells_Downsample <- function(
 
   # set num_cells if value is "min"
   if (num_cells == "min") {
-    cli_inform(message = c("The number of cells was set to {.val min}, returning {.field {min_cells}} cells per identity class (equal to size of smallest identity class {.val {min_cell_ident}}).",
-                           "Total of {.field {min_cells * length(x = idents_all)}} cells across whole object."))
+    cli_inform(message = c("The number of cells was set to {.val min}, returning {.field {min_cells}} cells per identity class (equal to size of smallest identity class(es): {.val {min_cell_ident}}).",
+                           "{col_green({symbol$tick})} Total of {.field {min_cells * length(x = idents_all)}} cells across whole object."))
     num_cells <- min_cells
   }
 
