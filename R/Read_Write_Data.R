e91fb014abaa2fbea9a9e5a9f79571dc83e09b62
Samuel Marsh <38284410+samuel-marsh@users.noreply.github.com>
2025-11-12 11:17:41 -0500

    remove future support for patch

--- R/Read_Write_Data.R
+++ R/Read_Write_Data.R
@@ -209,10 +209,8 @@ Create_CellBender_Merged_Seurat <- function(
 #' support file prefixes and altered loop by Samuel Marsh for scCustomize (also previously posted as
 #' potential PR to Seurat GitHub).
 #'
-#' @import future
 #' @import parallel
 #' @import pbapply
-#' @importFrom future.apply future_lapply
 #' @importFrom Matrix readMM
 #' @importFrom utils read.delim txtProgressBar setTxtProgressBar read.table
 #'
